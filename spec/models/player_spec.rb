ce6eafa6fee51c9fa714d026b7f798a47029499e
James T. Perreault <james@perreau.lt>
2015-08-25 11:03:25 -0400

    stub expected model validation specs

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "is invalid" do
+    it "without a handle"
+  end
 end
