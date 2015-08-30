13e42deab30e2d6d61d268ff7d79621209d8a9db
James T. Perreault <james@perreau.lt>
2015-08-30 12:00:13 -0400

    game validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,13 @@
 class Game < ActiveRecord::Base
   belongs_to :player
   belongs_to :level
+
+  validates_presence_of :player, :level
+  validate :player_cannot_have_multiple_games_active
+
+  def player_cannot_have_multiple_games_active
+    if Game.where(player: self.player).where(active: true).any?
+      errors.add(:player, "has an active game")
+    end
+  end
 end
