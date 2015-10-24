fc7b7243642cce9b6d9148e4e7a350f48fef7e41
James T. Perreault <james@perreau.lt>
2015-10-24 14:19:32 -0400

    not a great seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Level.delete_all
+l1 = Level.create!(position: 1, description: 'ground level')
+l2 = Level.create!(position: 2, description: 'second level')
+l3 = Level.create!(position: 3, description: 'third level')
+l4 = Level.create!(position: 4, description: 'top of the tower')
+
+Situation.delete_all
+Situation.create!(level: l1, description: 'you get hit by a goat', health_change: -10)
+Situation.create!(level: l1, description: 'you are trampled', health_change: -20)
+Situation.create!(level: l2, description: "you've been kicked by Billy", health_change: -13)
+Situation.create!(level: l2, description: 'you are ignored by the goats', health_change: 0)
+Situation.create!(level: l3, description: 'you fall off the tower', health_change: -100)
+Situation.create!(level: l3, description: 'a goat kisses you', health_change: 10)
+Situation.create!(level: l4, description: 'kicked!', health_change: -15)
+Situation.create!(level: l4, description: 'gored!', health_change: -30)
