7bce8927217c23a81e0a14386357472aff35e2a2
James T. Perreault <james@perreau.lt>
2015-10-24 14:08:32 -0400

    add get_situation method to level model

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,4 +1,10 @@
 class Level < ActiveRecord::Base
   validates_presence_of :position
   validates_uniqueness_of :position
+
+  has_many :situations
+
+  def get_situation
+    situations.order("RANDOM()").first
+  end
 end
