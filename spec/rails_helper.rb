6f815150cfe9c82d98c260465a96bcad6fa8c310
James T. Perreault <james@perreau.lt>
2016-01-07 15:37:47 -0500

    add fixtures (begin removing factories)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,7 +32,7 @@ RSpec.configure do |config|
   config.before(:suite) do
     begin
       DatabaseCleaner.start
-      FactoryGirl.lint
+      #FactoryGirl.lint
     ensure
       DatabaseCleaner.clean
     end
@@ -44,6 +44,8 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
