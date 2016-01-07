42d9fb1f2dbd8beae885ac440e9db0d479d43e24
James T. Perreault <james@perreau.lt>
2016-01-06 20:33:53 -0500

    stub for next batch of specs to complete

--- spec/requests/game_spec.rb
+++ spec/requests/game_spec.rb
@@ -1,13 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe 'Game creation', type: :request do
-  context 'with valid data' do
-    it 'creates a game' do
-      post '/games', { player: { handle: 'jtperreault' } }
-      expect(response.status).to eq(201)
+RSpec.describe 'Game requests', type: :request do
+  context 'creating a game' do
+    context 'with valid data' do
+      it 'creates a game' do
+        post '/games', { player: { handle: 'jtperreault' } }
+        expect(response.status).to eq(201)
+      end
     end
+
+    context 'with invalid data'
+    context 'with an active game'
+    context 'without a identifiable player'
   end
 
-  context 'with an active game'
-  context 'without a identifiable user' # aka a new user
+  context 'updating a game' do
+    it 'continues'
+    it 'ends'
+    it 'replies with correct errors'
+  end
 end
