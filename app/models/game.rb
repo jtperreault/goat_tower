3989687dc5cb802fd33b14d30e2aa936d9fa7f1f
James T. Perreault <jperreault@terakeet.com>
2020-11-01 11:23:19 -0500

    refactor: spacing

--- app/models/game.rb
+++ app/models/game.rb
@@ -9,7 +9,7 @@ class Game < ActiveRecord::Base
   validate  :player_cannot_have_multiple_games_active, on: :create
 
   delegate :description, to: :level, prefix: true
-  delegate :position, to: :level, prefix: true
+  delegate :position,    to: :level, prefix: true
 
   def continue
     apply_situation(next_level.get_situation)
